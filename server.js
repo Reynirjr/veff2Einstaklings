6286e96fad5084433855affa058beacb58c5e0d5
Benjamín <brj46@hi.is>
2025-02-23 14:13:39 +0000

    static síður fyrir login, groups, og lög og bætti nav

--- server.js
+++ server.js
@@ -10,5 +10,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 const indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
 
+const authRoutes = require('./routes/auth');
+app.use('/', authRoutes);
+
+const groupRoutes = require('./routes/groups');
+app.use('/', groupRoutes);
+
+const songRoutes = require('./routes/songs');
+app.use('/', songRoutes);
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
